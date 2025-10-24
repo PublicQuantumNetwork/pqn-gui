6d26e79c31a750059c0189e92abce52aa48a1cf5
marcosf2 <marcosf2@illinois.edu>
2025-10-24 15:02:35 -0500

    Removed unnecessary code

--- src/app/chsh/page1/layout.tsx
+++ src/app/chsh/page1/layout.tsx
@@ -17,31 +17,12 @@ export default function RootLayout(props: { children: React.ReactNode }) {
   
   
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', marginRight: 'auto', marginLeft: 'auto' }}>
-        <InitColorSchemeScript attribute="class" />
-        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <ThemeProvider theme={theme}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            {/* <ModeSwitch /> */}
-            <PageRedirectProvider>
+    <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{ minHeight: '40em', backgroundRepeat: 'no-repeat', width: '100%', backgroundPosition: 'center' }}>
+      {props.children}
+      <RedirectArrow direction="forward">
+        <Icon sx={{height:'30px'}}><ArrowForwardIosIcon /></Icon>
+      </RedirectArrow>
+    </Stack>
 
-
-                {/* </Stack>*/}
-                <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{ minHeight: '40em', backgroundRepeat: 'no-repeat', width: '100%', backgroundPosition: 'center' }}>
-                  {/*<RedirectArrow direction="back">
-                    {<Icon sx={{height:'30px'}}><ArrowBackIosNewIcon /></Icon>}
-                  </RedirectArrow>*/}
-                  {props.children}
-                  <RedirectArrow direction="forward">
-                    <Icon sx={{height:'30px'}}><ArrowForwardIosIcon /></Icon>
-                  </RedirectArrow>
-                </Stack>
-            </PageRedirectProvider>
-          </ThemeProvider>
-        </AppRouterCacheProvider>
-      </body>
-    </html>
   );
 }

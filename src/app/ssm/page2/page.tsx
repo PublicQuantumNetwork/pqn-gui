b39039572bf2239acbf42b8d282884b1ddd1d98d
marcosf2 <marcosf2@illinois.edu>
2026-01-30 11:02:14 -0600

    refactor ssm/page2

--- src/app/ssm/page2/page.tsx
+++ src/app/ssm/page2/page.tsx
@@ -6,6 +6,7 @@ import Typography from '@mui/material/Typography';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { requestFollower } from '@/calls';
+import Whobit from '@/components/Whobit';
 
 export default function MyComponent() {
   const router = useRouter();
@@ -30,160 +31,82 @@ export default function MyComponent() {
   }, [router]);
 
   return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
+    <Container maxWidth="lg" sx={{ my: 4 }}>
+      <Stack direction="row" alignItems="center" sx={{ width: '100%' }}>
+        <Whobit variant="left-wing-up" speechBubbleHeight="250px">
+          <Box sx={{ fontSize: '1em' }}>
+            {hasError ? (
+              <p>
+                There was an error connecting to the other computer. Please
+                press the `START OVER` button and try again.
+              </p>
+            ) : wasAccepted === false ? (
+              <p>
+                Other computer has said no to playing the game, please press the
+                `START OVER` button and try again
+              </p>
+            ) : (
+              <>
+                <p>
+                  Asking your friend to join. Please accept the popup that
+                  appears in the other computer.
+                </p>
+                <p>
+                  If no popup appeared please press the `START OVER` button and
+                  try again.
+                </p>
+              </>
+            )}
+          </Box>
+        </Whobit>
+
         <Stack
-          display="flex"
           flexDirection="column"
-          position="relative"
-          sx={{ width: '100%' }}
+          flex={1}
+          alignItems="center"
+          justifyContent="center"
+          sx={{ paddingLeft: '150px' }}
         >
-          <Stack
-            direction="row"
-            sx={{
-              minHeight: '8em',
-              justifyContent: 'left',
-              alignItems: 'center', // Align items to the center of the row
-            }}
-          >
-            <Stack
-              display="flex"
-              flexDirection="column"
-              position="relative"
-              sx={{ width: '50%' }}
-            >
-              <Box
-                component="img"
-                src="/images/speech-bubble-white-small.png"
-                alt="Whobit welcomes you"
-                sx={{
-                  width: 'auto',
-                  height: '18em',
-                  backgroundRepeat: 'no-repeat',
-                  backgroundSize: 'contain',
-                  backgroundPosition: 'left',
-                }}
-              />
-
-              <Typography
-                variant="h5"
-                component="h1"
-                sx={{
-                  position: 'absolute',
-                  top: '23%',
-                  left: '50%',
-                  transform: 'translate(-50%, -50%)',
-                  color: '#000000',
-                  width: '75%',
-                }}
-              >
-                {hasError ? (
-                  <p>
-                    There was an error connecting to the other computer. Please
-                    press the `START OVER` button and try again.
-                  </p>
-                ) : wasAccepted === false ? (
-                  <p>
-                    Other computer has said no to playing the game, please press
-                    the `START OVER` button and try again
-                  </p>
-                ) : (
-                  <>
-                    <p>
-                      Asking your friend to join. Please accept the popup that
-                      appears in the other computer.
-                    </p>
-                    <p>
-                      If no popup appeared please press the `START OVER` button
-                      and try again.
-                    </p>
-                  </>
-                )}
-              </Typography>
-
-              <Box
-                component="img"
-                src="/images/whobit-left-wing-up.png"
-                alt="Whobit welcomes you"
-                sx={{
-                  width: '18em',
-                  height: 'auto',
-                  backgroundRepeat: 'no-repeat',
-                  backgroundSize: 'contain',
-                  backgroundPosition: 'left',
-                }}
-              />
-            </Stack>
-
-            <Stack
-              display="flex"
-              flexDirection="column"
-              position="relative"
+          {hasError ? (
+            <Box
+              component="img"
+              src="/images/broken-computer.png"
+              alt="Error - request denied"
               sx={{
-                width: '50%',
-                justifyContent: 'center',
-                alignItems: 'flex-end',
-                pr: 4,
+                width: '200px',
+                height: 'auto',
+              }}
+            />
+          ) : wasAccepted === false ? (
+            <Typography
+              sx={{
+                fontSize: '150px',
               }}
             >
-              <Box
+              😞
+            </Typography>
+          ) : (
+            <>
+              <CircularProgress
+                size={80}
+                thickness={4}
                 sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
+                  color: 'black',
+                }}
+              />
+              <Typography
+                variant="h6"
+                sx={{
+                  mt: 2,
+                  color: 'black',
                 }}
               >
-                {hasError ? (
-                  <Box
-                    component="img"
-                    src="/images/broken-computer.png"
-                    alt="Error - request denied"
-                    sx={{
-                      width: '200px',
-                      height: 'auto',
-                    }}
-                  />
-                ) : wasAccepted === false ? (
-                  <Typography
-                    sx={{
-                      fontSize: '150px',
-                    }}
-                  >
-                    😞
-                  </Typography>
-                ) : (
-                  <>
-                    <CircularProgress
-                      size={80}
-                      thickness={4}
-                      sx={{
-                        color: 'black',
-                      }}
-                    />
-                    <Typography
-                      variant="h6"
-                      sx={{
-                        mt: 2,
-                        color: 'black',
-                      }}
-                    >
-                      Waiting for response...
-                    </Typography>
-                  </>
-                )}
-              </Box>
-            </Stack>
-          </Stack>
+                Waiting for response...
+              </Typography>
+            </>
+          )}
         </Stack>
-      </Box>
+      </Stack>
     </Container>
   );
 }
